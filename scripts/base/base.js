43aee251589ce18e511ca17c8b8ea3f0c3b9ad80
Hrvoje <hrvojekuvezdic1@gmail.com>
2025-12-23 21:43:18 +0100

    Display scroll to top button when scrolling a bit

---
+++ scripts/base/base.js
@@ -0,0 +1,19 @@
+const gotoStart = document.getElementById('gotoStart');
+
+window.addEventListener('scroll', () => {
+    if (!gotoStart) return;
+
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    const docHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+    const scrolledRatio = docHeight > 0 ? scrollTop / docHeight : 0;
+
+    if (scrolledRatio > 0.2) {
+        gotoStart.style.display = 'block';
+    }
+    else {
+        gotoStart.style.display = 'none';
+    }
+});
